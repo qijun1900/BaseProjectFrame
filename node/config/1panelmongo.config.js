47b7f39423daa81642c2f9ce4728bf609120c691
qijun1900 <3308837335@qq.com>
2025-04-12 17:50:50 +0800

    first commit

---
+++ node/config/1panelmongo.config.js
@@ -0,0 +1,8 @@
+//1panel MongoDB配置文件
+module.exports ={
+    username:'mongo_cG37de_example',
+    password:'mongo_FG2M6e_example',
+    host:'172.18.0.2',
+    sport:'27017',
+    databasename:'databasename_example'
+}
