7f1f30ede296174327a48a8ce17c04bd468be702
qijun1900 <3308837335@qq.com>
2025-07-14 11:30:05 +0800

    User逻辑功能完善

---
+++ AdminWeb/src/components/ReuseComponents/Pagination.vue
@@ -0,0 +1,24 @@
+<template>
+    <el-pagination 
+    v-model:current-page="currentPage"
+    background 
+    layout="total, sizes, prev, pager, next, jumper"
+    :total="1000" 
+    @size-change="handleSizeChange"
+    @current-change="handleCurrentChange"
+    />
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const currentPage = ref(5)
+
+const handleSizeChange = (val) => {
+  // 修改提示信息为中文
+  console.log(`每页 ${val} 条数据`)
+}
+const handleCurrentChange = (val) => {
+  // 修改提示信息为中文
+  console.log(`当前页码: ${val}`)
+}
+</script>
--- AdminWeb/src/views/Users/Users.vue
+++ AdminWeb/src/views/Users/Users.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <el-card style="border-radius: 4px" shadow="never">
+        <transition name="slide-up">
+        <el-card style="border-radius: 4px" shadow="never"  v-show="showSearch">
                 <el-row :gutter="30">
                     <el-col :span="7">
                         <el-form-item label="输入条件:" style="margin-bottom: 0;">
@@ -43,29 +44,51 @@
                     </el-col>
                 </el-row>
         </el-card>
+        </transition>
         <div class="eidt-card">
             <el-card style="border-radius: 4px," shadow="never" >
                 <div class="edit-btn">
                     <el-row :gutter="200">
                         <el-col :span="18"> 
-                            <el-button type="success" plain>新建用户</el-button>
-                            <el-button type="danger" plain>删除用户</el-button>   
-                            <el-button type="warning" plain>导出用户</el-button>
+                            <el-button 
+                                type="success" 
+                                plain
+                                @click="handleAddUser">新建用户</el-button>
+                            <el-button 
+                                type="danger" 
+                                plain
+                                @click="handleDeleteChooseUser">删除用户</el-button>   
+                            <el-button 
+                            type="warning" 
+                            plain
+                            @click="handleExportUser">导出用户</el-button>
                         </el-col> 
                         <el-col :span="6"> 
                             <Tooltip content="隐藏搜索">
                                 <template #description>
-                                    <el-button type="primary" :icon="Hide" circle />
+                                    <el-button 
+                                        type="primary" 
+                                        :icon="Hide" 
+                                        circle 
+                                        @click="HandleHideSearch"/>
                                 </template>
                             </Tooltip>
                             <Tooltip content="开启/关闭斑马纹">
                                 <template #description>
-                                    <el-button type="primary" :icon="Open" circle />
+                                    <el-button 
+                                        type="primary" 
+                                        :icon="Open" 
+                                        circle 
+                                        @click="handleOpenStripe"/>
                                 </template>
                             </Tooltip>
                             <Tooltip content="刷新表格">
                                 <template #description>
-                                    <el-button type="primary" :icon="RefreshRight" circle />
+                                    <el-button 
+                                        type="primary" 
+                                        :icon="RefreshRight" 
+                                        circle 
+                                        @click="handleRefreshTable" />
                                 </template>
                             </Tooltip>
                         </el-col> 
@@ -77,7 +100,9 @@
                     <el-table 
                         :data="tableData" 
                         style="width: 100%" 
-                        @selection-change="handleSelectionChange">
+                        @selection-change="handleSelectionChange"
+                        max-height="690"
+                        :stripe="IsOpenStripe">
                         <el-table-column type="selection" width="55" />
                         <el-table-column 
                             type="index" 
@@ -120,24 +145,44 @@
                         </el-table-column>
                         <el-table-column label="操作">
                             <template  #default="scope">
-                                <el-button>
+                                <el-button 
+                                    type="primary" 
+                                    plain
+                                    @click="handleEdit(scope.row)">
                                     编辑
                                 </el-button>
-                                <el-button>
+                                <el-button
+                                    type="danger"
+                                    plain
+                                    @click="handleDelete(scope.row)">
                                     删除
+                                </el-button>
+                                <el-button
+                                    type="info"
+                                    plain
+                                    @click="handleMore(scope.row)">
+                                    更多
                                 </el-button>
                             </template>
                         </el-table-column>
                     </el-table>
                 </div>
+                <div class="pagination">
+                    <Pagination/>
+                </div>
             </el-card>
+           
         </div>
     </div>
 </template>
 <script setup>
 import { ref } from 'vue'
-import { Search ,RefreshRight,CloseBold, Hide, Open} from '@element-plus/icons-vue'
+import { Search, RefreshRight, CloseBold, Hide ,Open} from '@element-plus/icons-vue' 
 import Tooltip from '@/components/ReuseComponents/Tooltip.vue'
+import Pagination from '@/components/ReuseComponents/Pagination.vue'
+
+const showSearch = ref(true)
+const IsOpenStripe = ref(false)
 const input1 = ref('')
 const input2 = ref('')
 const input3 = ref('')
@@ -216,8 +261,123 @@ const tableData = [
     gender: 1,
     role: 2,
     status: 1
+  },
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+    gender: 1,
+    role: 1,
+    status: 1
+  },
+  {
+    date: '2016-05-02',
+    name: 'Lucy',
+    address: 'No. 189, Grove St, Los Angeles',
+    gender: 2,
+    role: 2,
+    status: 0
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+    gender: 1,
+    role: 1,
+    status: 1
+  },
+   {
+    date: '2024-03-01',
+    name: '张伟',
+    address: '北京市朝阳区建国路',
+    gender: 1,
+    role: 1,
+    status: 1
+  },
+  {
+    date: '2024-03-02', 
+    name: '王芳',
+    address: '上海市浦东新区',
+    gender: 2,
+    role: 2,
+    status: 0
+  },
+  {
+    date: '2024-03-03',
+    name: '李强',
+    address: '广州市天河区体育西路',
+    gender: 1,
+    role: 2,
+    status: 1
+  },
+  {
+    date: '2024-03-04',
+    name: '赵敏',
+    address: '深圳市福田区',
+    gender: 2,
+    role: 1,
+    status: 1
+  },
+  {
+    date: '2024-03-05',
+    name: '陈伟',
+    address: '杭州市西湖区',
+    gender: 0,
+    role: 2,
+    status: 0
+  },
+  {
+    date: '2024-03-20',
+    name: '周涛',
+    address: '成都市锦江区',
+    gender: 1,
+    role: 2,
+    status: 1
   }
 ]
+
+
+//是否开启斑马纹
+const handleOpenStripe = () => {
+    IsOpenStripe.value = !IsOpenStripe.value
+}
+//刷新表格
+const HandleHideSearch = () => {
+    showSearch.value = !showSearch.value
+}
+//刷新表格
+const handleRefreshTable = () => {
+    // 这里可以添加刷新表格的逻辑，例如重新获取数据
+    console.log('刷新表格')
+}
+
+// 新增用户
+const handleAddUser = () => {
+    console.log('新增用户')
+}
+// 删除选择用户
+const handleDeleteChooseUser = () => {
+    console.log('删除用户')
+}
+// 导出用户
+const handleExportUser = () => {
+    console.log('编辑用户')
+}
+
+//编辑用户
+const handleEdit = (row) => {
+    console.log('编辑用户', row)
+}
+//删除表格用户
+const handleDelete = (row) => {
+    console.log('删除用户', row)
+}
+
+//handleMore
+const handleMore = (row) => {
+    console.log('更多操作', row)
+}
+
 
 
 // 处理选中事件
@@ -236,7 +396,27 @@ const handleSelectionChange = (val) => {
 .edit-table{
     margin-top: 10px;
 }
+.pagination{
+    margin-top: 15px;
+    display: flex; 
+    justify-content: center; 
+    align-items: center;
+}
 :deep(.el-table__header th) {
     background-color: #f5f7fa !important;
 }
+/* 添加滑动过渡效果 */
+.slide-up-enter-active,
+.slide-up-leave-active {
+    transition: all 0.3s ease-in-out;
+}
+
+.slide-up-enter-from,
+.slide-up-leave-to {
+    transform: translateY(-20px);
+    opacity: 0;
+}
 </style>
+
+
+
